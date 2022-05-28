6babbacb12198596afa65e54ab0ee01b1863326a
Wiliam Correa <wilcorrea@gmail.com>
2022-05-28 14:24:37 -0300

    feature: create voting domain

--- api/routes/http.php
+++ api/routes/http.php
@@ -3,6 +3,7 @@
 use App\Http\Info\HealtcheckAction;
 use Slim\App;
 use App\Domains\Inspiration\Adapters\Http\InspireAction;
+use App\Domains\Voting\Adapters\Http\VoteLocalAction;
 use App\Http\Error\NotFound;
 
 /**
@@ -12,6 +13,10 @@ use App\Http\Error\NotFound;
  */
 return static function (App $app): void {
     $app->get('/healthcheck', HealtcheckAction::class);
+
     $app->get('/inspire', InspireAction::class);
+
+    $app->post('/vote', VoteLocalAction::class);
+
     $app->any('/{path:.*}', NotFound::class);
 };
