3df0222842e81b0ac8c8840d31b9652f2792863a
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2022-05-29 01:54:08 -0300

    Update deploy.sh

--- stress-test/deploy.sh
+++ stress-test/deploy.sh
@@ -1,5 +1,5 @@
 # crie um repo no ECR com o nome de votacao-gatling (ou qq outro)
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin <sua-conta-aws>.dkr.ecr.us-east-1.amazonaws.com
 docker build -t votacao-gatling .
-docker tag votacao-gatling:latest 750321567594.dkr.ecr.us-east-1.amazonaws.com/votacao-gatling:latest
+docker tag votacao-gatling:latest <sua-conta-aws>.dkr.ecr.us-east-1.amazonaws.com/votacao-gatling:latest
 docker push <sua-conta-aws>.dkr.ecr.us-east-1.amazonaws.com/votacao-gatling:latest
